58ceb9dcd10e3007e6c8241979eca93dff148637
Mohin Goyal <mohiingoyal@gmail.com>
2025-11-09 16:01:27 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+function changeColor() {document.body.style.backgroundColor="lightgreen";}
+function changeColor1() {document.body.style.backgroundColor="lightblue";}
+function changeColor2() {document.body.style.backgroundColor="lavender"}
